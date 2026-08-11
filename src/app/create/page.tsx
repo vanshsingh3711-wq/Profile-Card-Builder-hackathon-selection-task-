fda0a61690e0ce38725cf418c2e0b862db57b5c7
Tenka04 <sujalsangle@gmail.com>
2026-08-11 19:46:58 +0530

    Reapply "feat: add tree assets and implement identity screen with navigation pages"
    This reverts commit 0a4ad201d7f3b0931675f93786975dd1fb0fd98d.

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -13,11 +13,20 @@ export default function CreatePage() {
       <div className="absolute top-[-10%] right-[-10%] w-[50vw] h-[50vw] rounded-full bg-hh-pink/10 blur-[120px] pointer-events-none" />
       <div className="absolute bottom-[-10%] left-[-10%] w-[60vw] h-[60vw] rounded-full bg-hh-yellow/5 blur-[120px] pointer-events-none" />
 
+      {/* Tree SVG Background Layer */}
+      <div className="absolute bottom-[-5%] left-0 right-0 h-[60vh] pointer-events-none flex justify-between items-end overflow-hidden z-0 opacity-40">
+        <img src="/branding/trees/transparent_asset_01.svg" alt="" className="h-full w-auto object-contain object-bottom -ml-20" />
+        <img src="/branding/trees/transparent_asset_03.svg" alt="" className="h-[80%] w-auto object-contain object-bottom -ml-10" />
+        <img src="/branding/trees/transparent_asset_04.svg" alt="" className="h-[90%] w-auto object-contain object-bottom -mr-10" />
+        <img src="/branding/trees/bush.svg" alt="" className="h-[60%] w-auto object-contain object-bottom -mr-20" />
+      </div>
+
       {/* 
         The z-10 ensures the glows stay behind the UI.
         BuilderFlow handles its own max-widths and padding.
+        We use [&>div]:!bg-transparent to ensure BuilderFlow's solid background doesn't cover the trees.
       */}
-      <div className="relative z-10 w-full flex-1 flex flex-col">
+      <div className="relative z-10 w-full flex-1 flex flex-col [&>div]:!bg-transparent">
         <BuilderFlow />
       </div>
     </main>
